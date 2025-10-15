008c0e5d092c533766018670048d8ae106a96288
josuealban <mjalban@sudamericano.edu.ec>
2025-10-15 10:14:45 -0500

    create peronalizacion

--- src/estudiantes/estudiantes/dto/create-estudiante.dto.ts
+++ src/estudiantes/estudiantes/dto/create-estudiante.dto.ts
@@ -1,1 +1,12 @@
-export class CreateEstudianteDto {}
+export class CreateEstudianteDto {
+    nombre: string;
+    apellido: string;
+    edad: number;
+    correoinstitucional: string;
+    identificacion: string;
+    telefono: string;
+    direccion: string;
+    genero: string;
+    fechanacimiento: Date;
+    estado: boolean;
+}
